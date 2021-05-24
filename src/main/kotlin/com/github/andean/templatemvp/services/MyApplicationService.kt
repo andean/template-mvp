0a1616ca033e962b1824352a75340bbb2fe946ff
GitHub Action <action@github.com>
2021-05-24 03:09:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/andean/templatemvp/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/andean/templatemvp/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.andean.templatemvp.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.andean.templatemvp.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/andean/templatemvp/services/MyProjectService.kt
+++ src/main/kotlin/com/github/andean/templatemvp/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.andean.templatemvp.services
 
+import com.github.andean.templatemvp.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
